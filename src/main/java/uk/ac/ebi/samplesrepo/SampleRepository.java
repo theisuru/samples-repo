7f48d484cbe1d69c5cf96fb924acfcd336827dc6
Karoly Erdos <karoly@ebi.ac.uk>
2022-04-27 10:41:27 +0100

    Add sample retrieval

--- src/main/java/uk/ac/ebi/samplesrepo/SampleRepository.java
+++ src/main/java/uk/ac/ebi/samplesrepo/SampleRepository.java
@@ -3,4 +3,5 @@ package uk.ac.ebi.samplesrepo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SampleRepository extends JpaRepository<Sample, String> {
+    Sample findByAccession(String accession);
 }
